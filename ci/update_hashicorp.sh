aaee3dcfadff5cddbe661f34057d5fe85246a70a
Timothée Ravier <tim@siosm.fr>
2023-03-30 12:06:44 +0200

    ci: Add support for nomad-driver-podman updates

--- ci/update_hashicorp.sh
+++ ci/update_hashicorp.sh
@@ -38,7 +38,50 @@ main() {
         name="${p}"
         base="https://releases.hashicorp.com/${name}"
 
-        current_version="$(eval "$(grep VERSION ${name}/Containerfile | head -1 | cut -f2 -d\  )"; echo $VERSION)"
+        current_version="$(eval "$(grep 'VERSION' ${name}/Containerfile | head -1 | cut -f2 -d\  )"; echo ${VERSION})"
+
+        project_id="$(get_id "${name}")"
+        version="$(curl "https://release-monitoring.org/api/v2/versions/?project_id=${project_id}" | jq --raw-output '.stable_versions[0]')"
+
+        echo "Project: ${name} - Current: $current_version -> Latest: $version"
+        if [[ "${current_version}" == "${version}" ]]; then
+            continue
+        fi
+
+        url_sha="${base}/${version}/${name}_${version}_SHA256SUMS"
+        url_sig="${base}/${version}/${name}_${version}_SHA256SUMS.sig"
+
+        rm -fv "${name}_${version}_SHA256SUMS" "${name}_${version}_SHA256SUMS.sig"
+
+        echo "[+] Downloading ${name} ${version}"
+        wget "${url_sha}"
+        wget "${url_sig}"
+
+        echo "[+] Verifying ${name} ${version}"
+        gpg --verify "${name}_${version}_SHA256SUMS.sig"
+
+        new_sha="$(grep 'linux_amd64' "${name}_${version}_SHA256SUMS")"
+
+        sed -i "0,/VERSION=.*$/{s//VERSION=\"${version}\"/}" "${name}/Containerfile"
+        sed -i "0,/SHA256=.*$/{s//SHA256=\"${new_sha}\"/}" "${name}/Containerfile"
+
+        git add "${name}/Containerfile"
+        git commit --message "${name}: Update to ${version}"
+
+        rm -v "${name}_${version}_SHA256SUMS" "${name}_${version}_SHA256SUMS.sig"
+    done
+
+    # Special case for nomad's podman driver
+    projects=(
+        'nomad'
+        'nomad-fedora'
+    )
+
+    for p in "${projects[@]}"; do
+        name="nomad-driver-podman"
+        base="https://releases.hashicorp.com/${name}"
+
+        current_version="$(eval "$(grep 'DRIVER_VERSION' ${p}/Containerfile | head -1 | cut -f2 -d\  )"; echo ${DRIVER_VERSION})"
 
         project_id="$(get_id "${name}")"
         version="$(curl "https://release-monitoring.org/api/v2/versions/?project_id=${project_id}" | jq --raw-output '.stable_versions[0]')"
@@ -62,11 +105,11 @@ main() {
 
         new_sha="$(grep linux_amd64 "${name}_${version}_SHA256SUMS")"
 
-        sed -i "0,/VERSION=.*$/{s//VERSION=\"${version}\"/}" "${name}/Containerfile"
-        sed -i "0,/SHA256=.*$/{s//SHA256=\"${new_sha}\"/}" "${name}/Containerfile"
+        sed -i "0,/DRIVER_VERSION=.*$/{s//DRIVER_VERSION=\"${version}\"/}" "${p}/Containerfile"
+        sed -i "0,/DRIVER_SHA256=.*$/{s//DRIVER_SHA256=\"${new_sha}\"/}" "${p}/Containerfile"
 
-        git add "${name}/Containerfile"
-        git commit --message "${name}: Update to ${version}"
+        git add "${p}/Containerfile"
+        git commit --message "${p}: Update ${name} to ${version}"
 
         rm -v "${name}_${version}_SHA256SUMS" "${name}_${version}_SHA256SUMS.sig"
     done
